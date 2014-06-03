a4b4d6271efdd2a8a58bb669b98d7a8ce8044c71
Laurent Pouilloux <lolo@pouilloux.org>
2014-06-03 17:00:02 +0200

    Removing the bash parser

--- funk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PYTHONHOMEPATH="/home/lpouilloux/.local/"$(python -c "import sys,os; print os.sep.join(['lib', 'python' + sys.version[:3], 'site-packages'])")
-export PYTHONPATH="$PYTHONHOMEPATH${PYTHONPATH:+:${PYTHONPATH}}"
-
-exec ./funk "$@"
